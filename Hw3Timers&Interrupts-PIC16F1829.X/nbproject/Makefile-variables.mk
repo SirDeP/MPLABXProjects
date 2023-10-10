688c057af9557dd4b853de99c614c1f7e908ac2b
SirDeP <biogene.53418@gmail.com>
2023-10-10 14:22:24 +0200

    Rename&upload current proj

---
+++ Hw3Timers&Interrupts-PIC16F1829.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=Hw3Timers_Interrupts-PIC16F1829.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/Hw3Timers_Interrupts-PIC16F1829.X.production.hex
